ad82f5a05a5aa26b2aee92b949b193070c8b7235
Ykaro Roberto <orakysec@gmail.com>
2026-07-08 16:46:31 -0300

    feat(ui): add Facebook Pixel tracking to Landing3D

--- frontend/src/pages/Landing3D.tsx
+++ frontend/src/pages/Landing3D.tsx
@@ -22,6 +22,24 @@ export default function Landing3D() {
     };
   }, []);
 
+  // Facebook Pixel
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.innerHTML = `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1036050992396935');
+      fbq('track', 'PageView');
+    `;
+    document.head.appendChild(script);
+  }, []);
+
   return (
     <div
       style={{
@@ -36,6 +54,9 @@ export default function Landing3D() {
         overflowX: 'hidden',
       }}
     >
+      <noscript>
+        <img height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=1036050992396935&ev=PageView&noscript=1" />
+      </noscript>
       <div style={{ maxWidth: 800, width: '100%', margin: '0 auto', textAlign: 'center' }}>
         {/* Header Badge */}
         <div
